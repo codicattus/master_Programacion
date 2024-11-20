f693ce1aa8d1f2c0e118e82ae91a76ca6ca1e2da
codicattus <codicattus@gmail.com>
2024-11-20 13:22:09 +0100

    ejemplo in, is

---
+++ ejemplo_in.py
@@ -0,0 +1,50 @@
+ciudades = ["Barcelona", "Roma", "París", "Londres"]
+letras = ["a", "b", ["cc", "dd"], "e", "f"]
+
+for ciudad in ciudades:
+    print(ciudad)
+
+# Resultado: 
+#
+# Barcelona
+# Roma
+# París
+# Londres
+
+for letra in letras:
+    print(letra)
+
+# Resultado: 
+#
+# a
+# b
+# ['cc', 'dd']
+# e
+# f
+
+print(len(letras))
+
+# Resultado: 
+#
+# 5
+
+for i in range(len(ciudades)):
+    print(ciudades[i])
+
+# Resultado: 
+#
+# Barcelona
+# Roma
+# París
+# Londres
+
+for i in range(len(letras)):
+    print(letras[i])
+
+# Resultado: 
+#
+# a
+# b
+# ['cc', 'dd']
+# e
+# f
---
+++ ejemplo_is.py
@@ -0,0 +1,20 @@
+lista = []
+texto = ""
+
+if type(lista) is list:
+    print("lista es un objeto de la clase list")
+else:
+    print(f"lista es un objetos de la clase {type(lista)}")
+
+# Resultado :
+#
+# lista es un objeto de la clase list
+
+if type(texto) is list:
+    print("Lista es un objeto de la clase list")
+else:
+    print(f"lista es un objetos de la clase {type(texto)}")
+
+# Resultado :
+#
+# lista es un objetos de la clase <class 'str'>
