ad18a0e345efff31cda916a09b0e2208cd69b85c
codicattus <codicattus@gmail.com>
2024-11-20 13:20:54 +0100

    number methods

---
+++ 3 - number/number_types_3.py
@@ -0,0 +1,13 @@
+num_complex = 4+2j
+
+print(num_complex.real)
+print(num_complex.imag)
+print(complex(10,4))
+
+# ----------------------------------
+# Resultado:
+# 
+# 4.0
+# 2.0
+# (10+4j)
+# ----------------------------------
