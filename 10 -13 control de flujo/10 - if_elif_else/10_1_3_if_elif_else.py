acf9fc1da548edc7c134fb1917317982a65e3c17
codicattus <codicattus@gmail.com>
2024-12-04 12:51:51 +0100

    Cambios if_lse y emulación switch-case

--- 10 -13 control de flujo/10 - if_elif_else/10_1_3_if_elif_else.py
+++ 10 -13 control de flujo/10 - if_elif_else/10_1_3_if_elif_else.py
@@ -1,4 +1,7 @@
-
+# Operador ternario
+# Expresión condicional ternaria
+# "if-else" ternario
+# "if expresion"
 
 resultado = "verdadero" if 5>4 else "falso"
 print(resultado)
@@ -12,6 +15,8 @@ a = 5
 b = 3
 c = 7
 
+# Expresión booleana compuesta
+
 if a >= b & b <= c:
     print(f"Se cumple la condición")
 else:
@@ -21,6 +26,8 @@ if a >= b and b <= c:
     print(f"Se cumple la condición")
 else:
     print("No se cumple la condición")    
+
+# Comparación encadenada
 
 if a >= b <= c:
     print(f"La variable a es mayor que b y b es menor que c")
--- 10 -13 control de flujo/10 - if_elif_else/10_2_emulacion_Switch_case.py
+++ 10 -13 control de flujo/10 - if_elif_else/10_2_emulacion_Switch_case.py
@@ -1,34 +1,56 @@
-def switch(case):
-    switcher = {
-        1: "Caso 1",
-        2: "Caso 2",
-        3: "Caso 3"
-    }
-    return switcher.get(case, "Caso por defecto")
-
-print(switch(1))
-print(switch(4)) 
-
-# Resultado :
-#
-# Caso 1
-# Caso por defecto
-#
-
-def if_switcher(caso):
-    if caso == 1:
-        return "Caso 1"
-    elif caso == 2:
-        return "Caso 2"
-    elif caso == 3:
-        return "Caso 3"
+def if_switcher(tecla):
+    if tecla == 1:
+        return "tecla 1"
+    elif tecla == 2:
+        return "tecla 2"
+    elif tecla == 3:
+        return "tecla 3"
+    elif tecla == 4:
+        return "tecla 4"
+    elif tecla == 5:
+        return "tecla 5"
+    elif tecla == 6:
+        return "tecla 6"
+    elif tecla == 7:
+        return "tecla 7"
+    elif tecla == 8:
+        return "tecla 8"
+    elif tecla == 9:
+        return "tecla 9"
+    elif tecla == 0:
+        return "tecla 0"
     else:
-        return "Caso por defecto"
+        return "tecla por defecto"
 
 
 print(if_switcher(2))
 
 # Resultado :
 #
-# Caso 2
+# tecla 2
+#
+
+
+def switch(case):
+    switcher = {
+        1: "tecla 1",
+        2: "tecla 2",
+        3: "tecla 3",
+        4: "tecla 4",
+        5: "tecla 5",
+        6: "tecla 6",
+        7: "tecla 7",
+        8: "tecla 8",
+        9: "tecla 9",
+        0: "tecla 0"
+    }
+    return switcher.get(case, "tecla por defecto")
+
+print(switch(1))
+print(switch(4)) 
+
+# Resultado :
+#
+# tecla 1
+# tecla por defecto
 #
