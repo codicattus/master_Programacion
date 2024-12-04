9dc61b6304a9ce05b841f395a6feb44278549b1f
codicattus <codicattus@gmail.com>
2024-12-04 13:09:08 +0100

    Variaciones en otros lenguajes

--- 10 -13 control de flujo/10 - if_elif_else/10 _1_1_if_elif_else.py
+++ 10 -13 control de flujo/10 - if_elif_else/10 _1_1_if_elif_else.py
@@ -34,3 +34,103 @@ else:
 # Esto solo se mostrará si el número es 0
 #
 
+# En otros lenguajes de programación
+
+# Javascript
+
+"""
+let numero = 5;
+
+if (numero > 10) 
+{
+    console.log("El número es mayor que 10.");
+} else if (numero > 5) 
+{
+    console.log("El número es mayor que 5 pero menor o igual a 10.");
+} else 
+{
+    console.log("El número es menor o igual a 5.");
+}
+"""
+
+# PHP
+
+"""
+$numero = 5;
+
+if ($numero > 10) 
+{
+    echo "El número es mayor que 10.";
+} elseif ($numero > 5) 
+{
+    echo "El número es mayor que 5 pero menor o igual a 10.";
+} else 
+{
+    echo "El número es menor o igual a 5.";
+}
+"""
+
+# Java
+
+"""
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        int numero = 5;
+
+        if (numero > 10) 
+        {
+            System.out.println("El número es mayor que 10.");
+        } else if (numero > 5) 
+        {
+            System.out.println("El número es mayor que 5 pero menor o igual a 10.");
+        } else 
+        {
+            System.out.println("El número es menor o igual a 5.");
+        }
+    }
+}
+"""
+
+# C++
+
+"""
+#include <iostream>
+
+int main() {
+    int numero = 5;
+
+    if (numero > 10) 
+    {
+        std::cout << "El número es mayor que 10." << std::endl;
+    } else if (numero > 5) 
+    {
+        std::cout << "El número es mayor que 5 pero menor o igual a 10." << std::endl;
+    } else 
+    {
+        std::cout << "El número es menor o igual a 5." << std::endl;
+    }
+
+    return 0;
+}
+"""
+
+# Rust
+
+"""
+fn main() {
+    let numero = 5;
+
+    if numero > 10 
+    {
+        println!("El número es mayor que 10.");
+    } else if numero > 5 
+    {
+        println!("El número es mayor que 5 pero menor o igual a 10.");
+    } else 
+    {
+        println!("El número es menor o igual a 5.");
+    }
+}
+"""
--- 10 -13 control de flujo/11 - for/11_1_for.py
+++ 10 -13 control de flujo/11 - for/11_1_for.py
@@ -49,3 +49,121 @@ for kilos, fruta in frutas:
 # Cantidad: 12kg. de peras.
 # Cantidad: 3kg. de sandías.
 # Cantidad: 14kg. de naranjas.
+
+
+
+
+
+# En otros lenguajes de programación
+
+# Javascript
+
+""" 
+let numeros = [1, 2, 3, 4, 5];
+
+// Bucle for clásico
+for (let i = 0; i < numeros.length; i++) 
+{
+    console.log(numeros[i]);
+}
+
+// Bucle for...of
+for (let numero of numeros) 
+{
+    console.log(numero);
+}
+
+// Bucle for...in
+for (let indice in numeros) 
+{
+    console.log(numeros[indice]);
+}
+ """
+
+# PHP
+
+"""
+$numeros = array(1, 2, 3, 4, 5);
+
+
+// Bucle for clásico
+for ($i = 0; $i < count($numeros); $i++) 
+{
+    echo $numeros[$i] . "\n";
+}
+
+// Bucle foreach
+foreach ($numeros as $numero) 
+{
+    echo $numero . "\n";
+}
+"""
+
+# Java
+
+"""
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        // Bucle for clásico
+        for (int i = 0; i < numeros.length; i++) 
+        {
+            System.out.println(numeros[i]);
+        }
+
+        // Bucle enhanced for (foreach)
+        for (int numero : numeros) 
+        {
+            System.out.println(numero);
+        }
+    }
+}
+"""
+
+# C++
+
+"""
+#include <iostream>
+
+int main() 
+{
+    int numeros[] = {1, 2, 3, 4, 5};
+
+    // Bucle for clásico
+    for (int i = 0; i < sizeof(numeros)/sizeof(numeros[0]); i++) 
+    {
+        std::cout << numeros[i] << std::endl;
+    }
+
+    // Bucle range-based for (disponible desde C++11)
+    for (int numero : numeros) 
+    {
+        std::cout << numero << std::endl;
+    }
+
+    return 0;
+}
+"""
+
+# Rust
+
+"""
+fn main() {
+    let numeros = [1, 2, 3, 4, 5];
+
+    // Bucle for clásico en Rust
+    for numero in numeros.iter() 
+    {
+        println!("{}", numero);
+    }
+
+    // Bucle con índice
+    for (i, numero) in numeros.iter().enumerate() 
+    {
+        println!("Índice: {}, Valor: {}", i, numero);
+    }
+}
+"""
