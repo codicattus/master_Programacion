97d54fa65804607515e2cbb7c3645b8a2cf13bdb
codicattus <codicattus@gmail.com>
2024-12-18 13:56:45 +0100

    lambda functions

---
+++ 15 - functions/functions_A_lambda.py
@@ -0,0 +1,21 @@
+# Definiendo una función lambda para duplicar un número
+duplicar = lambda x: x * 2
+
+# Usando la función lambda
+resultado = duplicar(5)
+print(resultado)
+
+
+sumar = lambda x, y: x + y
+
+resultado = sumar(3, 5)
+print(resultado)
+
+
+notas_trimestre = [(3, 4, 8), (4, 5, 5), (5, 6, 6)]
+lista_ordenada = sorted(
+    notas_trimestre, 
+    key=lambda x: (x[0] + x[1] + x[2]) / 3, 
+    reverse=True
+)
+print(lista_ordenada)
