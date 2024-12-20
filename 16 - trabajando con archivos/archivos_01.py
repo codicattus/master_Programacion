19e6a5de4669e79f5ab720b2abbd9c16bcb59b4d
codicattus <codicattus@gmail.com>
2024-12-20 14:10:28 +0100

    Modificación archivos_01

--- 16 - trabajando con archivos/archivos_01.py
+++ 16 - trabajando con archivos/archivos_01.py
@@ -1,10 +1,15 @@
-archivo = "Mitología griega.txt" 
+from pathlib import Path
+
+archivo = "Mitología griega.txt"
 # Con solo el nombre del archivo es probable que algunos IDE nos de algún problema
 
+print(archivo)
 leer_archivo = open(archivo, "r", encoding="UTF-8")
 contenido = leer_archivo.read()  # Lee TODO el contenido del archivo de golpe
 print(contenido)
 leer_archivo.close() # Es importante cerrar el archivo después de usarlo para liberar los recursos del sistema.
 
 
+# archivo = Path("Mitología griega.txt")
+# print(archivo.read_text())
 
